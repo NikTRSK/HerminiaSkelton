43fd538a5607bdd787d6cc86567baa1ae9b7aa28
Juntao Shen <Juntaosh@usc.edu>
2016-11-13 14:06:56 -0800

    added implementations

---
+++ HerminaSkelton/src/client/loginGUI.java
@@ -0,0 +1,149 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class loginGUI extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private BackGroundMusic music;
+	private JLabel loginLable;
+	private JLabel error;
+	private JTextField username;
+	private JTextField userpassword;
+	private JButton login, createAccount, settings, guest;
+	private GridBagConstraints mGridBagConst;
+
+	public loginGUI(){
+		
+		initializeComponents();
+		createGUI();
+		addEvents();
+//		music.gamestart();
+	}
+	
+	private void initializeComponents(){
+		music = new BackGroundMusic();
+		loginLable = new JLabel("Please Login");
+		loginLable.setFont(new Font("Serif", Font.BOLD, 25));
+		error = new JLabel("");
+		error.setFont(new Font("Serif", Font.BOLD, 15));
+		error.setVisible(false);
+		error.setFont(new Font("Serif", Font.BOLD, 25));
+		username = new JTextField("username");
+		username.setFont(new Font("Serif", Font.BOLD, 15));
+		username.setForeground(Color.gray);
+		userpassword = new JTextField("password");
+		userpassword.setFont(new Font("Serif", Font.BOLD, 15));
+		userpassword.setForeground(Color.gray);
+		login = new JButton("   login   ");
+		login.setFont(new Font("Serif", Font.BOLD, 15));
+		login.setBorderPainted(false);
+		login.setContentAreaFilled(false);
+		login.setOpaque(true);
+		login.setEnabled(false);
+		createAccount = new JButton("Create Account");
+		createAccount.setFont(new Font("Serif", Font.BOLD, 15));
+		createAccount.setBorderPainted(false);
+		createAccount.setContentAreaFilled(false);
+		createAccount.setOpaque(true);
+		createAccount.setEnabled(false);
+		settings = new JButton("Settings");
+		settings.setFont(new Font("Serif", Font.BOLD, 15));
+		settings.setBorderPainted(false);
+		settings.setContentAreaFilled(false);
+		settings.setOpaque(true);
+		guest = new JButton("Guest Login");
+		guest.setFont(new Font("Serif", Font.BOLD, 15));
+		guest.setBorderPainted(false);
+		guest.setContentAreaFilled(false);
+		guest.setOpaque(true);
+	}
+	
+	private void createGUI(){
+		setSize(800,620);
+		setLocation(750,100);
+		setBackground();
+		setLayout(new GridBagLayout());
+		setTitle("Welcome");
+		mGridBagConst = new GridBagConstraints();
+		mGridBagConst.weightx = 1.0;
+		mGridBagConst.gridwidth = java.awt.GridBagConstraints.RELATIVE;
+		mGridBagConst.gridy = 0;
+		mGridBagConst.insets = new Insets(20,500,100,80);
+		add(loginLable,mGridBagConst);
+		
+		mGridBagConst.gridy = 1;
+		mGridBagConst.insets = new Insets(0,500,30,80);
+		error.setVisible(true);
+		add(error,mGridBagConst);
+		
+		mGridBagConst.gridy = 2;
+		mGridBagConst.ipadx = 200;
+		mGridBagConst.ipady = 20;
+		mGridBagConst.insets = new Insets(0,500,10,80);
+		add(username,mGridBagConst);
+		
+		mGridBagConst.gridy = 3;
+		add(userpassword,mGridBagConst);
+		
+		mGridBagConst.gridy = 4;
+		mGridBagConst.ipadx = 120;
+		mGridBagConst.ipady = 10;
+		add(login,mGridBagConst);
+		
+		mGridBagConst.gridy = 5;
+		mGridBagConst.ipadx = 120;
+		mGridBagConst.ipady = 20;
+		add(createAccount,mGridBagConst);
+		
+		mGridBagConst.gridy = 6;
+		add(settings,mGridBagConst);
+		
+		mGridBagConst.gridy = 7;
+		add(guest,mGridBagConst);
+	}
+	
+	private void setBackground(){
+		try {
+		    final Image backgroundImage = javax.imageio.ImageIO.read(new File(Constants.resourceFolderbg + "background.jpg"));
+		    setContentPane(new JPanel(new BorderLayout()) {
+				private static final long serialVersionUID = -1315795587311609680L;
+
+				@Override public void paintComponent(Graphics g) {
+		            g.drawImage(backgroundImage, 0, 0, null);
+		        }
+		    });
+		} catch (IOException e) {
+			JPanel error = new JPanel();
+			JOptionPane.showMessageDialog(error,
+					"Asset Corruption",
+					"File Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
+	private void addEvents(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	public static void main(String [] args) {
+		new loginGUI().setVisible(true);
+	}
+}
+
--- HerminaSkelton/src/utilities/Constants.java
+++ HerminaSkelton/src/utilities/Constants.java
@@ -5,4 +5,10 @@ import java.text.SimpleDateFormat;
 
 public class Constants {
 	public static final DateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+    public static final String resourceFolderbgm = "resources/bgm/";
+    public static final String resourceFolderbg = "resources/img/";
+    public static final String battlemusic = "battlestart.mp3";
+    public static final String casualmusic = "casualstart.mp3";
+    public static final String startmusic = "gamestart.mp3";
+    public static final String finalmusic = "finalstart.mp3";
 }
