b3c9a007f39691687b47e7a26ca68684b77adfbb
Jalin Marks <jalinmar@usc.edu>
2016-11-16 21:20:44 -0800

    gamegui update

--- HerminaSkelton/src/client/GameGUI.java
+++ HerminaSkelton/src/client/GameGUI.java
@@ -1,7 +1,10 @@
 package client;
 
 import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -9,12 +12,13 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class GameGUI extends JFrame{
-	private JPanel mainPanel;
+	private JPanel centerPanel;
 	private JPanel rightPanel;
 	private JPanel MapScreen;
+	private JPanel BattleScreen;
 	private JPanel miniMap;
 	private JPanel scoreDisplay;
-	//private ChatPanel chat;
+	private ChatPanel chat;
 	private JTextField score;
 	private GameClientListener clientListener;
 
@@ -27,27 +31,63 @@ public class GameGUI extends JFrame{
 	}
 	
 	private void initializeComponents(){
-		mainPanel = new JPanel();
-		rightPanel = new JPanel();
-		MapScreen = new JPanel();
+		centerPanel = new JPanel();
+		rightPanel = new JPanel();		
+		MapScreen = new JPanel();//new map.MapScreen(new Dimension(Toolkit.getDefaultToolkit().getScreenSize())); 
 		miniMap = new JPanel();
 		scoreDisplay = new JPanel();
-		//chat = new ChatPanel();
+		chat = new ChatPanel(clientListener);
 		score = new JTextField();
 		//clientListener = new GameClientListener();
 	}
 	
 	private void createGUI(){
-		mainPanel.setLayout(new BorderLayout());
+		setLayout(new BorderLayout());
 		
+		createCenterPanel();
+		createScorePanel();
+		createMiniMap();
+		createRightPanel();
+		
+		add(centerPanel, BorderLayout.CENTER);
+		add(rightPanel, BorderLayout.EAST);
+
+	}
+	
+	private void createMiniMap(){
+		
+	}
+	
+	private void createScorePanel(){
+		
+	}
+	
+	private void createRightPanel(){
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		
 		rightPanel.add(miniMap);
 		rightPanel.add(scoreDisplay);
+		rightPanel.add(chat);
+	}
+	
+	private void createCenterPanel(){
+		centerPanel.setLayout(new CardLayout());
+		centerPanel.add(createMapCard(), "map screen");
+		//centerPanel.add(createBattleCard(), "battle card");
+	}
+	
+	private JPanel createMapCard(){
+		map.MapScreen map = new map.MapScreen(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		MapScreen.add(map);
 		
-		mainPanel.add(MapScreen, BorderLayout.CENTER);
-		mainPanel.add(rightPanel, BorderLayout.EAST);
+		return MapScreen;
+	}
+	
+	private JPanel createBattleCard(){
+		BattleScreen battle = new BattleScreen(null);
+		BattleScreen.add(battle);
 		
-		add(mainPanel);
+		return BattleScreen;
 	}
 	
 }
