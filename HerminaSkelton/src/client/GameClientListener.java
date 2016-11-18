a0e141077a62352177a7c967013a15ef973ba041
Jalin Marks <jalinmar@usc.edu>
2016-11-18 10:53:59 -0800

    client listenr and gamgui update

--- HerminaSkelton/src/client/GameClientListener.java
+++ HerminaSkelton/src/client/GameClientListener.java
@@ -1,15 +1,25 @@
 package client;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
+
+import javax.swing.text.Utilities;
+
+import server.ServerListener;
+import utilities.DataPacket;
+import utilities.User;
 
 public class GameClientListener extends Thread{
 	private Socket mSocket;
 	private ObjectInputStream ois;
-	private PrintWriter pw;
+	private ObjectOutputStream oos;
+	//private PrintWriter pw;
 	private GameGUI mGameGUI;
+	private boolean ans = false;
 	
 	public GameClientListener(GameGUI gameGUI, Socket socket){
 		mSocket = socket;
@@ -20,17 +30,75 @@ public class GameClientListener extends Thread{
 		}
 	}
 	
-	public void sendMessage(String msg){
-		pw.print(msg);
-		pw.flush();
+	/*
+	 * send data to gameClientListener, receives response from server 
+	 * and subsequently allows gui to behave accordingly
+	 */
+	public boolean sendData(DataPacket<?> data){
+		try {
+			oos.writeObject(data);
+			oos.flush();
+			//should there be some way to wait until response is received from server?
+			//does this always return false?
+			return getAnswer();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}	
 	}
 	
-	public void run(){
-		//check for another game instance
-		GameGUI game;
+	//send msg to clientListener, doesn't send data that requires/expects a response from server
+	public void sendMessage(DataPacket<?> data){
 		try{
+			oos.writeObject(data);
+			oos.flush();
+		} catch (IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public boolean getAnswer(){
+		return ans;
+	}
+
+	
+	public void run(){		
+		try{
+			DataPacket<?> input = (DataPacket<?>)ois.readObject();			
+			String streamContent = input.getCommand();
+			//ServerListener listener = new ServerListener(mSocket);
 			while(true){
-				game = (GameGUI)ois.readObject();
+				if (streamContent == utilities.Commands.END_GAME){
+					//String endMsg = (String)input.getData();
+					mGameGUI.updateGameGUI(streamContent);
+				}
+				else if(streamContent == utilities.Commands.LOGIN_USER){
+					User user = (User)input.getData();
+					oos.writeObject(new DataPacket<User>(utilities.Commands.LOGIN_USER, user));
+				}
+				else if(streamContent == utilities.Commands.LOGOUT_USER){
+					//send user to server to be logged out
+					User user  = (User)input.getData();
+					oos.writeObject(new DataPacket<User>(utilities.Commands.LOGOUT_USER, user));
+					//or take command and update gameGUI accordingly
+					//String logoutMsg = (String)input.getData();
+					//mGameGUI.updateGameGUI();
+				}
+				else if (streamContent == utilities.Commands.CREATE_USER){
+					User user = (User)input.getData();
+					oos.writeObject(new DataPacket<User>(utilities.Commands.CREATE_USER, user));
+				}
+				else if(streamContent == utilities.Commands.AUTH_RESPONSE){
+					boolean authenticate = (boolean)input.getData();
+					ans = authenticate;
+				}
+				else if(streamContent == utilities.Commands.CREATE_RESPONSE){
+					boolean create = (boolean)input.getData();
+					ans = create;
+				}
+				else if(streamContent == utilities.Commands.START_GAME){
+					
+				}
 			}
 		}catch(IOException ioe){
 			System.out.println("Server communication failed");
@@ -42,7 +110,9 @@ public class GameClientListener extends Thread{
 	private boolean initializeVariables(){
 		try{
 			ois = new ObjectInputStream(mSocket.getInputStream());
-			pw = new PrintWriter(mSocket.getOutputStream());
+			//get socket's output stream and open PrintWriter
+			//pw = new PrintWriter(mSocket.getOutputStream());
+			oos = new ObjectOutputStream(mSocket.getOutputStream());
 		}catch (IOException ioe){
 			return false;
 		}
--- HerminaSkelton/src/client/GameGUI.java
+++ HerminaSkelton/src/client/GameGUI.java
@@ -2,7 +2,10 @@ package client;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 
 import javax.swing.BoxLayout;
@@ -20,12 +23,15 @@ public class GameGUI extends JFrame{
 	private ChatPanel chat;
 	private JTextField score;
 	private GameClientListener clientListener;
+	GridBagConstraints gbc;
+	CardLayout cards;
 
 	public GameGUI(){
 		super("Game");
 		setSize(600,600);
 		initializeComponents();
 		createGUI();
+		//cards.show(createMapCard(), "map screen");
 		setVisible(true);
 	}
 	
@@ -37,19 +43,21 @@ public class GameGUI extends JFrame{
 		scoreDisplay = new JPanel();
 		chat = new ChatPanel(clientListener);
 		score = new JTextField();
+		gbc = new GridBagConstraints();
+		cards = new CardLayout();
 		//clientListener = new GameClientListener();
 	}
 	
 	private void createGUI(){
-		setLayout(new BorderLayout());
+		setLayout(new GridBagLayout());
+		MapScreen.setBackground(Color.pink);
+		BattleScreen.setBackground(Color.orange);
 		
 		createCenterPanel();
 		createScorePanel();
 		createMiniMap();
 		createRightPanel();
-		
-		add(centerPanel, BorderLayout.CENTER);
-		add(rightPanel, BorderLayout.EAST);
+	
 
 	}
 	
@@ -63,30 +71,56 @@ public class GameGUI extends JFrame{
 	
 	private void createRightPanel(){
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		rightPanel.setBackground(Color.green);
+		gbc.fill = GridBagConstraints.BOTH;
+		//
+		gbc.gridx = 200;
+		gbc.gridy = 0;
 		
 		rightPanel.add(miniMap);
 		rightPanel.add(scoreDisplay);
 		rightPanel.add(chat);
+		
+		add(rightPanel, gbc);
 	}
 	
 	private void createCenterPanel(){
 		centerPanel.setLayout(new CardLayout());
+		centerPanel.setBackground(Color.blue);
 		centerPanel.add(createMapCard(), "map screen");
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
 		//centerPanel.add(createBattleCard(), "battle card");
+		
+		add(centerPanel, gbc);
 	}
 	
 	private JPanel createMapCard(){
-		map.MapScreen map = new map.MapScreen(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
+		map.MapScreen map = new map.MapScreen(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()),null);
 		MapScreen.add(map);
 		
 		return MapScreen;
 	}
 	
 	private JPanel createBattleCard(){
-		BattleScreen battle = new BattleScreen(null);
+		BattleScreen battle = new BattleScreen(null,null);
 		BattleScreen.add(battle);
 		
 		return BattleScreen;
+	}
+	
+	public void updateGameGUI(String whichUpdate){
+		if (whichUpdate == utilities.Commands.END_GAME){
+			//dialog pane that checks if user really wants to end game
+		}
+		else if(whichUpdate == utilities.Commands.LOGOUT_USER){
+			
+		}
+		else if(whichUpdate == utilities.Commands.MY_MOVE){
+			//move player?
+		}
+		
 	}
 	
 }
