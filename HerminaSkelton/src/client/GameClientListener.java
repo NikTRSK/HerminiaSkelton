bece79bd4a17f90e25292fe177c06b2e184c8051
NikTRSK <trsk.nick@gmail.com>
2016-11-20 12:19:56 -0800

    gcl changes

--- HerminaSkelton/src/client/GameClientListener.java
+++ HerminaSkelton/src/client/GameClientListener.java
@@ -18,6 +18,7 @@ import utilities.User;
 import utilities.Util;
 
 public class GameClientListener extends Thread{
+	private String userName;
 	private Socket mSocket;
 	private ObjectInputStream ois;
 	private ObjectOutputStream oos;
@@ -84,33 +85,8 @@ public class GameClientListener extends Thread{
 
 	public void login(User user) {
 		ans = null;
+		userName = user.getUsername();
 		sendData(new DataPacket<User>(utilities.Commands.LOGIN_USER, user));
-		System.out.println("in GC:");
-
-//		while (ans == null) {
-//			userLock.lock();
-//			try {
-//				userCondition.await();
-//			} catch (InterruptedException ie) {
-//				Util.printExceptionToCommand(ie);
-//			}
-//			userLock.unlock();
-//		}
-//		return ans;
-//		while (ans == null) {
-//			responseLock.lock();
-//			try {
-//				responseCondition.await();
-//			} catch (InterruptedException ie) {
-//				Util.printExceptionToCommand(ie);
-//			}
-//			responseLock.unlock();
-//		}
-//		return ans;
-//		
-//		while (ans == null) {}
-//		System.out.println("in GC: " + ans);
-//		return ans;
 	}
 	
 	public boolean create(User user) {
@@ -127,31 +103,14 @@ public class GameClientListener extends Thread{
 				DataPacket<?> input = (DataPacket<?>)ois.readObject();			
 				String streamContent = input.getCommand();
 				System.out.println("COMMAND::: " + streamContent);
-				//ServerListener listener = new ServerListener(mSocket);
-//				if (input != null) {
-//					System.out.println("Command: " + input.getCommand());
-//					loginGUI.processLogin((boolean)input.getData());
-//				}
+				
 				if (streamContent.equals(utilities.Commands.END_GAME)){
-					//String endMsg = (String)input.getData();
-//					mGameGUI.updateGameGUI(streamContent);
-				}
-				else if(streamContent.equals(utilities.Commands.LOGIN_USER)){
-					User user = (User)input.getData();
-					//oos.writeObject(new DataPacket<User>(utilities.Commands.LOGIN_USER, user));
+					
 				}
 				else if(streamContent.equals(utilities.Commands.LOGOUT_USER)){
-					//send user to server to be logged out
-					User user  = (User)input.getData();
-					//oos.writeObject(new DataPacket<User>(utilities.Commands.LOGOUT_USER, user));
-					//or take command and update gameGUI accordingly
-					//String logoutMsg = (String)input.getData();
-					//mGameGUI.updateGameGUI();
+					User user = (User)input.getData();
+					// call the logout on the gui
 				}
-//				else if (streamContent == utilities.Commands.CREATE_USER){
-//					User user = (User)input.getData();
-//					//oos.writeObject(new DataPacket<User>(utilities.Commands.CREATE_USER, user));
-//				}
 				else if(streamContent.equals(utilities.Commands.AUTH_RESPONSE)){
 					boolean authenticate = (boolean)input.getData();
 					loginGUI.processLogin(authenticate);
