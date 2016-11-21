9fd4385cd560185269ba508845243794728a14a0
Andrew Maney <amaney@usc.edu>
2016-11-20 21:36:29 -0800

    map stuff

---
+++ HerminaSkelton/src/map/NodeLabel.java
@@ -0,0 +1,16 @@
+package map;
+
+public class NodeLabel {
+	
+	public int zoneX, zoneY, x, y;
+	public String label;
+
+	public NodeLabel(int inZoneX, int inZoneY, int inX, int inY, String inLabel) {
+		zoneX = inZoneX;
+		zoneY = inZoneY;
+		x = inX;
+		y = inY;
+		label = inLabel;
+	}
+
+}
