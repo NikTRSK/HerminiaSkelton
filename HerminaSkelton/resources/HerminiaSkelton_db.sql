bedf08d35bb68daf54f4bea683e1072e8e79fea8
NikTRSK <trsk.nick@gmail.com>
2016-11-20 17:09:31 -0800

    fixed create on client side

--- HerminaSkelton/resources/HerminiaSkelton_db.sql
+++ HerminaSkelton/resources/HerminiaSkelton_db.sql
@@ -4,30 +4,13 @@ CREATE DATABASE HerminiaSkelton_DB;
 
 USE HerminiaSkelton_DB;
 
-CREATE TABLE UserAccount (
+CREATE TABLE users (
   userID int(11) primary key not null auto_increment,
   username varchar(50) not null,
-  password varchar(50) not null
-);
-
-CREATE TABLE UserScores (
-  scoreID int(11) primary key not null auto_increment,
+  password varchar(50) not null, 
   score1 int(11),
   score2 int(11),
   score3 int(11),
   score4 int(11),
   score5 int(11)
 );
-
-CREATE TABLE User (
-  'userID' INT NOT NULL,
-  'scoreID' INT NOT NULL,
-  PRIMARY KEY('userID', 'scoreID')
-);
-
-SELECT u.*, s.*
-FROM UserAccount u
-INNER JOIN User user
-ON user.userID = u.userID
-INNER JOIN UserScores s
-ON s.scoreID = user.scoreID
