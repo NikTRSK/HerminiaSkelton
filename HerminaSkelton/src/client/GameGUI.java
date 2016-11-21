83610a4a0868e48a67575ddf8c91d54d559de374
Jalin Marks <jalinmar@usc.edu>
2016-11-20 18:02:31 -0800

    Update GameGUI.java

--- HerminaSkelton/src/client/GameGUI.java
+++ HerminaSkelton/src/client/GameGUI.java
@@ -2,46 +2,45 @@ package client;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
+import java.awt.Dialog;
 import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 import AllCPs.CP;
+import utilities.BackGroundPanel;
 
 public class GameGUI extends JFrame{
+	private static final long serialVersionUID = -8312855782342576917L;
+	
 	private JPanel centerPanel;
 	private JPanel rightPanel;
-	private JPanel MapScreen;
-	private JPanel BattleScreen;
-	//private JPanel miniMap;
-	private JPanel scoreDisplay;
+	
 	private ChatPanel chat;
-	private JTextField score;
 	private GameClientListener clientListener;
 	private JMenuBar menuBar;
 	private JMenu menu;
 	GridBagConstraints gbc;
-	CardLayout cards;
 	Player beta;
 	private JDialog finalBattle;
-	private Vector<CP> cpInventory;
 	
-	private map.MapScreen map;//
-	private BattleScreen battle;//
+	private map.MapScreen map;
+	private BattleScreen battle;
 
 	public GameGUI(GameClientListener listener){
 		super("Game");
@@ -49,61 +48,61 @@ public class GameGUI extends JFrame{
 		setSize(new Dimension(Toolkit.getDefaultToolkit().getScreenSize()));
 		initializeComponents();
 		createGUI();
+		
+		// fullscreen stuff
+//		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+//		setUndecorated(true);
+//		gd.setFullScreenWindow(this);
+		
 		setVisible(true);
+		switchToMap(false);
+		
+		//GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        //this.setUndecorated(true);
+        //gd.setFullScreenWindow(this);
 	}
 	
 	private void initializeComponents(){
 		
 		rightPanel = new JPanel();		
-		MapScreen = new JPanel();
-		BattleScreen = new JPanel();
-		//miniMap = new JPanel();
-		scoreDisplay = new JPanel();
-		chat = new ChatPanel(clientListener);
-		score = new JTextField();
-		gbc = new GridBagConstraints();
-		cards = new CardLayout();
-		centerPanel = new JPanel(cards);
+		
 		beta = new Player("Elgin");
+		
+		chat = new ChatPanel(clientListener);
+		gbc = new GridBagConstraints();
+		//cards = new CardLayout();
+		centerPanel = new JPanel(new CardLayout());
+		
 		finalBattle = new JDialog();
+		
 		menuBar = new JMenuBar();
 		menu = new JMenu("Inventory");
 	}
 	
 	private void createGUI(){
-		setLayout(new GridBagLayout());
+		//this.setLayout(new GridBagLayout());
+		this.setLayout(new BorderLayout());
+		
+		// Map Stuff.
 		map = new map.MapScreen(this,new Dimension(Toolkit.getDefaultToolkit().getScreenSize()),beta);
-		battle  = new BattleScreen(beta, this);
+		//int width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+		//int height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		//map.setPreferredSize(new Dimension((2*width)/3,height));
 		
-		MapScreen.add(map);
-		int width = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
-		int height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-		MapScreen.setPreferredSize(new Dimension((2*width)/3,height));
-		BattleScreen.setPreferredSize(new Dimension((2*width)/3,height));
-		BattleScreen.add(battle);
-		centerPanel.add(MapScreen, "card 1");		
-		centerPanel.add(BattleScreen, "card 2");
+		// Battle Stuff.
+		battle  = new BattleScreen(beta, this);		
+		//battle.setPreferredSize(new Dimension((2*width)/3,height));
+		centerPanel.setBackground(Constants.BACKGROUND_COLOR);
+		centerPanel.add(map, "card 1");		
+		centerPanel.add(battle, "card 2");
 		
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.weightx = .75;
-		gbc.gridwidth = GridBagConstraints.RELATIVE;	
-		gbc.gridheight = GridBagConstraints.REMAINDER;
-		add(centerPanel, gbc);
-		
+		this.add(centerPanel, BorderLayout.CENTER);
+		chat.setBorder(BorderFactory.createLineBorder(Constants.BACKGROUND_COLOR, 5));
 		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
-		//rightPanel.add(miniMap);
-		//rightPanel.add(scoreDisplay);
+		rightPanel.setBackground(Constants.BACKGROUND_COLOR);
 		rightPanel.add(chat);
 		
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.gridx = GridBagConstraints.RELATIVE;
-		gbc.gridy = 0;
-		gbc.weightx = .25;
-		gbc.gridheight = GridBagConstraints.REMAINDER;
-		gbc.gridwidth = GridBagConstraints.REMAINDER;
-				
-		add(rightPanel, gbc);
+		this.add(rightPanel, BorderLayout.EAST);
 		
 		menuBar.add(menu);
 		JMenuItem inventory = new JMenuItem("Show Inventory");
@@ -111,32 +110,69 @@ public class GameGUI extends JFrame{
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				JFrame showInventory = new JFrame("Inventory");
-				showInventory.setSize(600, 600);
-				showInventory.setVisible(true);
-				cpInventory = beta.getCP();			
+				JDialog dialog = new JDialog(GameGUI.this, Dialog.ModalityType.APPLICATION_MODAL);  
+				
+				Vector<CP> CPs = beta.getCP();
+				
+				JPanel CPHolder = new JPanel();
+				  CPHolder.setLayout(new BoxLayout(CPHolder, BoxLayout.X_AXIS));
+				  
+				  for(int i = 0; i < CPs.size(); i++){
+					  int a = i;
+					  
+					  JLabel chooseCP = new JLabel(CPs.get(i).getName()+" lvl "+CPs.get(i).getLevel());
+					  chooseCP.setBackground(Constants.BACKGROUND_COLOR2);
+					  chooseCP.setForeground(Constants.FONT_COLOR);
+					  chooseCP.setFont(Constants.GAMEFONT);
+					  
+					  JPanel CP = new BackGroundPanel(Constants.TYPE_BACKGROUNDS[CPs.get(i).getType()-1]);
+					  CP.setLayout(new BorderLayout());
+					  CP.add(new JLabel(CPs.get(i).getSprite()), BorderLayout.CENTER);
+					  CP.add(chooseCP, BorderLayout.SOUTH);
+					  CPHolder.add(CP);
+				  }
+				  
+				  
+				  JPanel dialogPanel = new JPanel();
+				  dialogPanel.setLayout(new BorderLayout());
+				  dialogPanel.setBackground(Constants.BACKGROUND_COLOR);
+				  dialogPanel.add(CPHolder, BorderLayout.CENTER);
+				  dialogPanel.add(new JLabel("Your CPs"), BorderLayout.NORTH);
+				  
+				  dialog.add(dialogPanel);
+				  dialog.pack();
+				  dialog.setVisible(true);
+				//JFrame showInventory = new JFrame("Inventory");
+				//showInventory.setSize(600, 600);
+				//showInventory.setVisible(true);
+				//cpInventory = beta.getCP();			
 			}
 			
 		});
-	
+		
 		menu.add(inventory);
 		setJMenuBar(menuBar);
-	
-		cards.show(centerPanel, "card 1");
 	}
 	
 	public void switchToBattle(){
+		CardLayout cards = (CardLayout)centerPanel.getLayout();
 		cards.show(centerPanel, "card 2");
 	}
 	
 	public void switchToMap(boolean dead){
 		if (dead == true){
 			map.setToCPCenter();
+			CardLayout cards = (CardLayout)centerPanel.getLayout();
+			cards.show(centerPanel, "card 1");
 		}
 		else{
 			map.renderAndPaint();
+			CardLayout cards = (CardLayout)centerPanel.getLayout();
 			cards.show(centerPanel, "card 1");
 		}
+		map.renderAndPaint();
+		map.setFocusable(true);
+		map.requestFocusInWindow();
 	}	
 	
 	public void timerout(){
