3960f53a21b7428cb5b8f16e647e1f4717422e9a
Andrew Maney <amaney@usc.edu>
2016-11-21 23:37:22 -0800

    listener

--- HerminaSkelton/src/client/GameClientListener.java
+++ HerminaSkelton/src/client/GameClientListener.java
@@ -154,6 +154,9 @@ public class GameClientListener extends Thread implements Serializable {
 				}
 				if (streamContent.equals(utilities.Commands.END_GAME)){
 					//mainGUI.endOfGame();
+					sendData(new DataPacket<Integer>(utilities.Commands.CHAT_MESSAGE, mGameGUI.getPlayerScore()));
+					EndGameGUI endGUI = new EndGameGUI(0, userName, "Name2", client.Constants.generateCP(1), 200, 300, true, this);
+					endGUI.setVisible(true);
 				}
 				else if(streamContent.equals(utilities.Commands.LOGOUT_USER)){
 					User user = (User)input.getData();
