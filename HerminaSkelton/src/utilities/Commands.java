09fe11fc39824a048392e1526406185fc88d873d
Nikica Trajkovski <NikTRSK@users.noreply.github.com>
2016-11-22 00:43:02 -0800

    Update Commands.java

--- HerminaSkelton/src/utilities/Commands.java
+++ HerminaSkelton/src/utilities/Commands.java
@@ -13,7 +13,7 @@ public class Commands {
 	public static final String END_GAME = "END_GAME"; //
 	public static final String ERROR_MESSAGE = "ERROR_MESSAGE"; // String
 	public static final String CHAT_MESSAGE = "CHAT_MESSAGE"; // Chat Message
-	public static final String GAME_SCORES = "GAME_SCORES"; // prob. ArrayList<Integer>
+	public static final String TOP_SCORES = "TOP_SCORES"; // prob. ArrayList<Integer>
 	public static final String TIMER_OUT = "TIMER_OUT"; // Boolean
 	public static final String PLAYER_ACTION = "PLAYER_ACTION"; // PlayerAction
 	public static final String GAME_MODE = "GAME_MODE"; // Integer ( 0 - single, 1 - multiplayer )
@@ -21,4 +21,5 @@ public class Commands {
 	public static final String FINAL_BATTLE = "FINAL_BATTLE"; // Integer
 	public static final String PLAYER_BEFORE_FB = "PLAYER_BEFORE_FB";
 	public static final String SEND_SCORE = "SEND_SCORE"; // sends player score
+	public static final String SINGLE_FINAL_BATTLE = "SINGLE_FINAL_BATTLE"; // null
 }
