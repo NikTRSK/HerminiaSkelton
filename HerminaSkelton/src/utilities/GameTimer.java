b177acd9931ad974842e6f60722ab320326a2f5b
Nikica Trajkovski <NikTRSK@users.noreply.github.com>
2016-11-27 19:41:34 -0800

    Update GameTimer.java

--- HerminaSkelton/src/utilities/GameTimer.java
+++ HerminaSkelton/src/utilities/GameTimer.java
@@ -3,25 +3,29 @@ package utilities;
 import java.io.Serializable;
 
 public class GameTimer extends Thread implements Serializable {
-	private static final long serialVersionUID = 6661280195671974593L;
-	private Integer timer;
-	GameInstance gameInstance;
-	
-	public GameTimer(GameInstance gi) {
-		timer = utilities.Constants.GAME_TIME*60+2;
-		this.gameInstance = gi;
-	}
-	
-	@Override
-	public void run() {
-		while (timer >= 1) {
-			try {
-				timer--;
-				gameInstance.updatePlayerTimers(timer);
-				Thread.yield();
-				Thread.sleep(1000);
-			} catch (InterruptedException ie) { utilities.Util.printExceptionToCommand(ie);	}
-		}
-		gameInstance.timerOut();
-	}
+  private static final long serialVersionUID = 6661280195671974593L;
+  private Integer timer;
+  GameInstance gameInstance;
+  
+  public GameTimer(GameInstance gi) {
+    timer = utilities.Constants.GAME_TIME*60+2;
+    this.gameInstance = gi;
+  }
+  
+  public void stopTimer() {
+    this.timer = -1; 
+  }
+  
+  @Override
+  public void run() {
+    while (timer >= 1) {
+      try {
+        timer--;
+        gameInstance.updatePlayerTimers(timer);
+        Thread.yield();
+        Thread.sleep(1000);
+      } catch (InterruptedException ie) { utilities.Util.printExceptionToCommand(ie); }
+    }
+    gameInstance.timerOut();
+  }
 }
